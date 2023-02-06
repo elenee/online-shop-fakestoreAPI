b682319ca7c998455967a18d8003a913b3d2513b
Elene Chikviladze <elenechikviladzewrk@gmail.com>
2023-02-06 15:18:08 +0400

    footer done

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="container1 grid1">
+        <div className="box">
+          <h3>STORE</h3>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+        </div>
+        <div className="box">
+          <h2>Recent Post</h2>
+          <div className="text">
+            <p>Leclerc frustrated over Ferrari 'changing' team orders decision</p>
+            <span>13 Nov 2022</span>
+          </div>
+        </div>
+        <div className="box">
+          <h2>Get In Touch</h2>
+          <p>Mail: <span>elenechikviladzewrk@gmail.com</span></p>
+        </div>
+      </div>
+      <div className="container legal">
+        <label>
+          Design and developed by <span>Elene Chikviladze</span>
+        </label>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
